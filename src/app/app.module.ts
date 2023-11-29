a690533548cd71fafde99bbd1fe1bcdac08daede
Rayan Silva <rayansl0302@gmail.com>
2023-11-29 16:05:15 -0300

    projeto completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//modules
+import { PagesModule } from './pages/pages.module';
+
+//components
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+//module
+import { RoutingModule } from './routing-module';
+import { SharedModule } from "../shared/shared.module";
+
+//pages
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+
+
+@NgModule({
+    declarations: [
+        HomeComponent,
+        DetailsComponent
+    ],
+    imports: [
+        CommonModule,
+        RoutingModule,
+        SharedModule
+    ]
+})
+export class PagesModule { }
